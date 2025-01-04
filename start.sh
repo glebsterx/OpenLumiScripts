4cec439b9e2f0f87bf77923e6cbe50ed6c3dee51
Gleb <glebsterx@gmail.com>
2025-01-05 02:14:02 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+wget https://github.com/glebsterx/OpenLumiScripts/archive/refs/heads/main.tar.gz -O /tmp/OpenLumi.tar.gz
+mkdir -p /tmp/OpenLumi
+tar -xvzf /tmp/OpenLumi.tar.gz -C /tmp/OpenLumi
+mv /tmp/OpenLumi/OpenLumiScripts-main/* /tmp/OpenLumi/
+rm -rf /tmp/OpenLumi/OpenLumiScripts-main
+sh /tmp/OpenLumi/setup.sh
